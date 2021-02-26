24420861dbba0e43d4d287d3f080725c904d6964
Lance <281239768@qq.com>
2021-02-26 15:43:09 +0800

    cmd: Support global debug flag instead of GIN_MODE env (#5)
    * cmd: Support global debug flag instead of GIN_MODE env
    
    * remove debug check in recovery middleware, always log stack when panic

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,13 @@ const (
 	Version     = "0.0.1"
 )
 
+// globalFlags handle flags for global command
+type globalFlags struct {
+	debug bool
+}
+
+var globalFlag = globalFlags{}
+
 var RootCmd = &cobra.Command{
 	Use:     Name,
 	Long:    Description,
@@ -26,6 +33,7 @@ func Init() error {
 }
 
 func initGlobalFlags() {
+	RootCmd.PersistentFlags().BoolVar(&globalFlag.debug, "debug", false, "enable debug or not")
 	// Overwrite the default help flag to free -h shorthand.
 	RootCmd.PersistentFlags().Bool("help", false, "help for this command")
 }
