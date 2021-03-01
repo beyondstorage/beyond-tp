74ef2fb245ccc44b3bda2c747ccc43660a461ba7
zhouyang <zhouyang@yunify.com>
2021-03-01 17:23:23 +0800

    init(front): Init front dev env

---
+++ static/js/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+import '../scss/index.scss';
+
+// moment.locale(Cookies.get('lang') || 'en');
+
+ReactDOM.render(
+  <div>
+    DM client page
+  </div>,
+  document.getElementById('root'),
+);
