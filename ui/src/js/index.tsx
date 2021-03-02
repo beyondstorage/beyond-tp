197588590ddaad09df6681a1cad46c891b19a152
周洋 <58585204@qq.com>
2021-03-02 17:04:58 +0800

    ui: Init frontend (#16)
    * init(front): Init front dev env
    
    * fix(fronted): Fixed the fronted env inited
    
    * feat(workflows): Add javascript workflows
    
    * refactor(fronted): Refactor fronted project inited
    
    * fix(workflow): Fixed the js workflow
    
    * fix(workflow): Fixed the js workflow
    
    Co-authored-by: zhouyang <zhouyang@yunify.com>

---
+++ ui/src/js/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import '../scss/index.scss';
+
+ReactDOM.render(
+  <div>
+    DM client page
+  </div>,
+  document.getElementById('root')
+);
