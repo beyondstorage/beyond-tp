29a4203473a14c56f8626e935cf6f5a13d674c4a
zhangdi <zhangdi@yunify.com>
2021-04-13 14:19:43 +0800

    ui: Refactor create task dialog style

--- ui/lib/widgets/dialog/index.dart
+++ ui/lib/widgets/dialog/index.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 class CommonDialog extends StatelessWidget {
   final String title;
   final Widget content;
-  final Widget footer;
+  final List<Widget> buttons;
   final Function onClose;
 
-  CommonDialog({this.title, this.content, this.footer, this.onClose});
+  CommonDialog({this.title, this.content, this.buttons, this.onClose});
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
+    final size = MediaQuery.of(context).size;
+    final contentHeight = size.height - 60 - 60 - 90;
+
+    return AlertDialog(
       title: Row(
         children: [
           Expanded(
@@ -29,11 +32,42 @@ class CommonDialog extends StatelessWidget {
         ],
       ),
       titlePadding: EdgeInsets.only(top: 16, right: 20, bottom: 32, left: 20),
+      content: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: contentHeight),
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: content,
+          ),
+        ),
+      ),
       contentPadding: EdgeInsets.all(0),
-      children: [
-        content,
-        footer,
+      actions: [
+        Container(
+          width: 600,
+          padding: EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: 44,
+          ),
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(3.0)),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, -1),
+                color: Color.fromRGBO(3, 5, 7, 0.08),
+                blurRadius: 3.0,
+              )
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: buttons,
+          ),
+        ),
       ],
+      actionsPadding: EdgeInsets.zero,
+      buttonPadding: EdgeInsets.zero,
+      insetPadding: EdgeInsets.zero,
     );
   }
 }
