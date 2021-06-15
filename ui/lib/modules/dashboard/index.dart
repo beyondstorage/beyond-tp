36bae7929880ded43c72c37e290e971ba72d6275
zhangdichn <zhangdichn@users.noreply.github.com>
2021-06-15 14:44:41 +0800

    ui: Add empty entrry list widget (#89)
    Co-authored-by: zhangdi <zhangdi@yunify.com>
    Co-authored-by: 周洋 <zhouyang@yunify.com>

--- ui/lib/modules/dashboard/index.dart
+++ ui/lib/modules/dashboard/index.dart
@@ -1,1 +1,46 @@
-import 'package:get/get.dart';import 'package:flutter/material.dart';import '../layout/index.dart';import '../../widgets/page_description/index.dart';import 'entry_list.dart';import 'controller.dart';class Dashboard extends StatelessWidget {  final DashboardController c = Get.put(DashboardController());  @override  Widget build(BuildContext context) {    c.getTasks();    return Layout(      child: Column(        children: [          PageDescription(            icon: Icons.source,            title: 'Tasks'.tr,            subtitle: "Create and manage your data migration tasks".tr,          ),          EntryList(),        ],      ),    );  }}
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+import '../layout/index.dart';
+import '../../widgets/page_description/index.dart';
+import '../../widgets/empty_entry_list/index.dart';
+
+import 'entry_list.dart';
+import 'create_task_dialog/index.dart';
+import 'controller.dart';
+
+class Dashboard extends GetView<DashboardController> {
+  final DashboardController c = Get.put(DashboardController());
+
+  @override
+  Widget build(BuildContext context) {
+    c.getTasks();
+
+    return Layout(
+      child: Column(
+        children: [
+          PageDescription(
+            icon: Icons.source,
+            title: 'Tasks'.tr,
+            subtitle: "Create and manage your data migration tasks".tr,
+          ),
+          Visibility(
+            visible: controller.tasks.value.length() == 0,
+            child: EmptyEntryList(
+              icon: Icons.source,
+              title: 'The task list is empty'.tr,
+              subTitle: 'Please click the button below to create a task'.tr,
+              buttonText: 'Create task'.tr,
+              onClick: () =>
+                  Get.dialog(CreateTaskDialog(getTasks: controller.getTasks)),
+            ),
+          ),
+          Visibility(
+            visible: controller.tasks.value.length() > 0,
+            child: EntryList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
