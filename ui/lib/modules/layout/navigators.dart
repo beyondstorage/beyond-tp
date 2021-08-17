f48a5e451714e8f9a45d6de95264f5dfad8cad88
zhangdichn <zhangdichn@gmail.com>
2021-08-17 16:59:36 +0800

    ui: Refactor navigators code style

--- ui/lib/modules/layout/navigators.dart
+++ ui/lib/modules/layout/navigators.dart
@@ -12,48 +12,49 @@ class Navigators extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: navigationWidth,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: SvgProvider(
-              'images/sidebar_graphics.svg',
-              size: Size(80.0, 1024.0),
-              color: rgba(52, 61, 190, 1),
-            ),
-            fit: BoxFit.fill,
+      width: navigationWidth,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: SvgProvider(
+            'images/sidebar_graphics.svg',
+            size: Size(80.0, 1024.0),
+            color: rgba(52, 61, 190, 1),
           ),
+          fit: BoxFit.fill,
         ),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 32.0),
-              child: Center(
-                child: GestureDetector(
-                  onTap: () => Get.toNamed("/"),
-                  child: MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: Text(
-                      'BTP',
-                      style: TextStyle(
-                        height: 1.0,
-                        fontSize: 32.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 32.0),
+            child: Center(
+              child: GestureDetector(
+                onTap: () => Get.toNamed("/"),
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: Text(
+                    'BTP',
+                    style: TextStyle(
+                      height: 1.0,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
             ),
-            SideLink(title: "Tasks".tr, icon: Icons.source, path: Routes.main),
-            SideLink(title: "Agents".tr, icon: Icons.dns, path: Routes.agents),
-            SideLink(
-              title: "Identities".tr,
-              icon: Icons.how_to_reg,
-              path: Routes.identities,
-            ),
-          ],
-        ));
+          ),
+          SideLink(title: "Tasks".tr, icon: Icons.source, path: Routes.main),
+          SideLink(title: "Agents".tr, icon: Icons.dns, path: Routes.agents),
+          SideLink(
+            title: "Identities".tr,
+            icon: Icons.how_to_reg,
+            path: Routes.identities,
+          ),
+        ],
+      ),
+    );
   }
 }
