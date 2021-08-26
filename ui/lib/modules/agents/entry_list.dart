ea611c1d96192d2eb8cc609ac2457c0550a10440
tthaogege <942519762@qq.com>
2021-08-26 10:46:06 +0800

    ui:agent review changes

--- ui/lib/modules/agents/entry_list.dart
+++ ui/lib/modules/agents/entry_list.dart
@@ -8,7 +8,6 @@ import 'controller.dart';
 import 'toolbar.dart';
 
 class EntryList extends GetView<AgentsController> {
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -17,30 +16,30 @@ class EntryList extends GetView<AgentsController> {
           child: Padding(
             padding: EdgeInsets.all(20),
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Toolbar(
-                    onClick: () {
-                      controller.getAgents(callBack: () {
-                        Message.success(
-                          context: Get.overlayContext as BuildContext,
-                          message: "Refresh successfully！",
-                        );
-                      });
-                      
-                    },
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Toolbar(
+                  onClick: () {
+                    controller.getAgents(callBack: () {
+                      Message.success(
+                        context: Get.overlayContext as BuildContext,
+                        message: "Refresh successfully！",
+                      );
+                    });
+                  },
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      ...controller.agents.value.agents.map((agent) => AgentItem(agent: agent))
+                    ],
                   ),
-                  Expanded(
-                      flex: 1,
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: [
-                          ...controller.agents.value.agents.map((agent) => AgentItem(agent: agent))
-                        ],
-                      )
-                  ),
-                ]),
+                ),
+              ],
+            ),
           ),
         ),
       )),
